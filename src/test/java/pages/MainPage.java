3fce1da1e56e11a3de6ee77e2f9b365f85a994b9
Sinan Karadavut <sinankaradavut84@gmail.com>
2020-10-16 16:43:41 -0400

    last update

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,8 +1,10 @@
 package pages;
 
+import net.bytebuddy.implementation.bind.annotation.FieldValue;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
 
 public class MainPage {
@@ -17,6 +19,68 @@ public class MainPage {
 
     @FindBy(partialLinkText = "Mobile Phones")
     public WebElement mobilePhone;
+
+
+    @FindBy(linkText = "Seller Sign in")
+    public WebElement sellerSignin;
+
+    @FindBy(id = "username")
+    public WebElement usernamebox;
+
+    @FindBy(id="password")
+    public WebElement passwordbox;
+
+    @FindBy(id="submit")
+    public WebElement logInButton;
+
+
+    @FindBy(xpath ="(//span[@class='icon_with_fon'])[3]")
+    public WebElement jewelryAccessories;
+
+
+    @FindBy(linkText ="Earrings")
+    public WebElement earrings;
+    @FindBy(linkText = "Wedding & Engagement Jewelry")
+    public WebElement wedding;
+    @FindBy(linkText = "Fine Jewelry" )
+    public WebElement fineJewelry;
+    @FindBy(linkText = "Beads & Jewelry Making")
+    public WebElement beadsJewelry;
+    @FindBy(linkText = "Necklaces & Pendants")
+    public WebElement necklacesPendants;
+    @FindBy(linkText = "Rings")
+    public WebElement rings;
+    @FindBy(linkText = "Bracelets & Bangles")
+    public WebElement braceletsBangles;
+    @FindBy(linkText = "Jewelry Sets & More")
+    public WebElement jewelrySetsMore;
+
+    @FindBy(linkText = "Sign In")
+    public WebElement signIn;
+    @FindBy(id = "email")
+    public WebElement email;
+    @FindBy(id = "password")
+    public WebElement password;
+    @FindBy(xpath = "//button[@value='Login']")
+    public WebElement login;
+    @FindBy(id="typeahead")
+    public WebElement searchbox;
+    @FindBy(xpath = "//i[@class='fa fa-search']")
+    public WebElement searchLogo;
+    @FindBy(xpath = "//img[@class='lazy img-responsive']")
+    public WebElement appleIpad;
+    @FindBy(xpath = "//a[@class='btn btn-default'][4]")
+    public WebElement gb256;
+    @FindBy(xpath = "//a[@class='btn btn-default'][3]")
+    public WebElement gb128;
+    @FindBy(xpath = "//a[@class='btn btn-default'][2]")
+    public WebElement gb64;
+    @FindBy(xpath = "//a[@class='btn btn-default'][1]")
+    public WebElement gb32;
+    @FindBy(xpath = "//h4[@class='pro_new_head']")
+    public WebElement infoText;
+
+
 
 
 
---
+++ src/test/java/stepdefinitions/GLB03_Sinan_StepDefinitions.java
@@ -0,0 +1,66 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.sl.In;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import pages.MainPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.util.List;
+
+public class GLB03_Sinan_StepDefinitions {
+    MainPage homePage=new MainPage();
+    @Given("user is on the homepage")
+    public void user_is_on_the_homepage(){
+        Driver.getDriver().get(ConfigReader.getProperty("glb_trader_url"));
+    }
+    @And("user click seller sign in")
+    public void  user_click_seller_sign_in(){
+        homePage.sellerSignin.click();
+    }
+    @And("user enter email")
+    public void user_enter_email(){
+        homePage.usernamebox.click();
+        homePage.usernamebox.sendKeys(ConfigReader.getProperty("seller_username"));
+    }
+    @And("user enter password")
+    public void user_enter_password(){
+        homePage.passwordbox.click();
+        homePage.passwordbox.sendKeys(ConfigReader.getProperty("seller_password"));
+    }
+    @And("user click on log in button")
+    public void user_click_in_log_in_button(){
+        homePage.logInButton.click();
+    }
+
+
+    @And("user on the homepage")
+    public void user_on_the_homepage() {
+        Driver.getDriver().navigate().back();
+        Driver.getDriver().navigate().back();
+    }
+
+
+    @When("click on jewelry accessories")
+    public void click_on_jewelry_accessories() {
+        homePage.jewelryAccessories.click();
+
+    }
+    @Then("verify is visible the all options under jewelery accessories")
+    public void verify_is_visible_the_all_options_under_jewelery_accessories(){
+       Assert.assertTrue(homePage.earrings.isDisplayed());
+        Assert.assertTrue(homePage.wedding.isDisplayed());
+        Assert.assertTrue(homePage.fineJewelry.isDisplayed());
+        Assert.assertTrue(homePage.beadsJewelry.isDisplayed());
+        Assert.assertTrue(homePage.necklacesPendants.isDisplayed());
+        Assert.assertTrue(homePage.rings.isDisplayed());
+        Assert.assertTrue(homePage.braceletsBangles.isDisplayed());
+        Assert.assertTrue(homePage.jewelrySetsMore.isDisplayed());
+    }
+}
